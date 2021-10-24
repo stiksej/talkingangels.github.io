beaa977fabd17e6e1249de61e12b2259bff70e31
stiksej <strzelczyk337@wp.pl>
2021-10-24 19:29:31 +0200

    j

--- assets/js/preloader.js
+++
@@ -1,9 +0,0 @@
-function loader(){
-    document.querySelector('.loader-container').classList.add('fade-out');
-  }
-  
-  function fadeOut(){
-    setInterval(loader, 5000);
-  }
-  
-  window.onload = fadeOut;
